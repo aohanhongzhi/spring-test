2aad5e14b7052f807e333a1556a704ab286a39a7
aohanhongzhi <aohanhongzhi@qq.com>
2019-05-19 11:43:02 +0800

    maven

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # spring-test
 ![Image text](https://github.com/aohanhongzhi/spring-test/blob/master/%E8%BF%90%E8%A1%8C%E5%90%8E%E7%9A%84%E6%88%AA%E5%9B%BE.png)
+
+<!-- https://mvnrepository.com/artifact/org.springframework/spring-context -->
+<dependency>
+    <groupId>org.springframework</groupId>
+    <artifactId>spring-context</artifactId>
+    <version>4.3.8.RELEASE</version>
+</dependency>
+
+<!-- https://mvnrepository.com/artifact/org.springframework/spring-core -->
+<dependency>
+    <groupId>org.springframework</groupId>
+    <artifactId>spring-core</artifactId>
+    <version>4.3.8.RELEASE</version>
+</dependency>
+
+<!-- https://mvnrepository.com/artifact/org.springframework/spring-beans -->
+<dependency>
+    <groupId>org.springframework</groupId>
+    <artifactId>spring-beans</artifactId>
+    <version>4.3.8.RELEASE</version>
+</dependency>
+
+<!-- https://mvnrepository.com/artifact/org.springframework/spring-expression -->
+<dependency>
+    <groupId>org.springframework</groupId>
+    <artifactId>spring-expression</artifactId>
+    <version>4.3.8.RELEASE</version>
+</dependency>
--- src/hxy/spring/test/MainApp.java
+++ src/hxy/spring/test/MainApp.java
@@ -1,28 +1,26 @@
 package hxy.spring.test;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
- 
+
 public class MainApp {
- 
+
 	public static void main(String[] args) {
- 
+		// 怎么一劳永逸，难道所有地方的调用都需要这样调用文件？
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		HelloWorld obj = (HelloWorld) context.getBean("helloworld");
-		System.out.println(obj);//自动把对象转成String，从而会调用类里面的toString方法。虽然有点扯,但是这样解释应该还算完美。
+		System.out.println(obj);// 自动把对象转成String，从而会调用类里面的toString方法。虽然有点扯,但是这样解释应该还算完美。
 		obj.setMessage("Today is sunny");
 		System.out.println(obj.getMessage());
 		System.out.println(obj);
 		System.out.println(obj.toString());
 	}
- 
+
 }
 /*
-环境配置参考:
-https://blog.csdn.net/csdnsjg/article/details/80152815
-
---------------------- 
-作者：英雄策马走天涯 
-来源：CSDN 
-原文：https://blog.csdn.net/qq_36360784/article/details/81103244 
-版权声明：本文为博主原创文章，转载请附上博文链接！
-*/
+ * 环境配置参考: https://blog.csdn.net/csdnsjg/article/details/80152815
+ * 
+ * --------------------- 作者：英雄策马走天涯 来源：CSDN
+ * 原文：https://blog.csdn.net/qq_36360784/article/details/81103244
+ * 版权声明：本文为博主原创文章，转载请附上博文链接！
+ */
